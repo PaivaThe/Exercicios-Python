5c33d11992cb7a12c671c4961703725d89ddc92e
Davi Paiva <83519832+PaivaThe@users.noreply.github.com>
2025-09-03 17:38:03 -0300

    Add files via upload

---
+++ 007.py
@@ -0,0 +1,8 @@
+#Programa que le o nome completo de uma pessoa 
+
+nome = str(input('Digite seu nome completo: ')).strip()
+
+print(f'{nome.upper()}')
+print(f'{nome.lower()}')
+print(f'{len(nome)-nome.count(' ')}')
+print(f'{nome.find(' ')}')
