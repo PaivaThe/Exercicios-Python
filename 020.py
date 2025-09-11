3a3179d147a308e09fc48db3b17c3775f3d7fe4a
Davi Paiva <83519832+PaivaThe@users.noreply.github.com>
2025-09-10 23:41:54 -0300

    Add files via upload

---
+++ 020.py
@@ -0,0 +1,10 @@
+num1 = int(input('Digite um número int: '))
+num2 = int(input('Digite outro número int: '))
+
+
+if num1 > num2:
+        print('O primeiro valkor é maior')
+elif num2 > num1:
+        print('O segundo valor é maior')
+elif num1 == num2:
+        print('Não existe valor maior, os dois são iguais')
